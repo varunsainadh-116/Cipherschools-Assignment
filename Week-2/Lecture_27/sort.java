7912f7cf1cb735555870c3baba45dd151676afae
Varun <korumillivarunsainadh@gmail.com>
2024-04-02 00:29:30 +0530

    lecture_27 done

---
+++ Week-2/Lecture_27/sort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class sort{
+    public static void slection(int[] arr){
+        int  n = arr.length;
+        for(int i = 0; i<n-1; i++){
+            int min = i;
+            for(int j = i+1; j<n; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the elements of the array: ");
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        slection(arr);
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
