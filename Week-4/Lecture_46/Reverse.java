5f9df5a3ed23c5383c2f59c3d0c763803bfe466a
Varun <korumillivarunsainadh@gmail.com>
2024-04-06 22:45:58 +0530

    Reversed the Linkedlist

---
+++ Week-4/Lecture_46/Reverse.java
@@ -0,0 +1,51 @@
+package Lecture_46;
+import java.util.Scanner;
+
+
+class Reverse {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int x) {
+            data = x;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Node head = null;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            head = insert(head, x);
+        }
+        int k = sc.nextInt();
+        System.out.println(nthNode(head, k));
+        sc.close();
+    }
+
+    static Node insert(Node head, int x) {
+        Node temp = new Node(x);
+        if (head == null)
+            return temp;
+        Node temp1 = head;
+        while (temp1.next != null)
+            temp1 = temp1.next;
+        temp1.next = temp;
+        return head;
+    }
+    static Node reversenode(Node head){
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
