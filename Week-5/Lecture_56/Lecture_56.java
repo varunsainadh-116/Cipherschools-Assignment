3b76c644e16df214b97f9938f2e16a78de5e3391
Varun <korumillivarunsainadh@gmail.com>
2024-04-15 01:00:14 +0530

    done

---
+++ Week-5/Lecture_56/Lecture_56.java
@@ -0,0 +1,103 @@
+import java.util.Stack;
+
+public class Lecture_56 {
+    public static int evaluatePrefixExpression(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = expression.length() - 1; i >= 0; i--) {
+            char c = expression.charAt(i);
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                switch (c) {
+                    case '+':
+                        stack.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        stack.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        stack.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        stack.push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+    public static int evaluatePostfixExpression(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                switch (c) {
+                    case '+':
+                        stack.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        stack.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        stack.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        stack.push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+    private static int precedence(char operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+    
+    public static String convertInfixToPostfix(String expression) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (Character.isDigit(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+        return postfix.toString();
+    }
+}
+
+
+
+
