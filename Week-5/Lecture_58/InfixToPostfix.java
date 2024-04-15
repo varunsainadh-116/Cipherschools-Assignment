f5eef51242ef50037a41c4253f93e4e15aad609e
Varun <korumillivarunsainadh@gmail.com>
2024-04-15 23:33:14 +0530

    InfixToPostfix

---
+++ Week-5/Lecture_58/InfixToPostfix.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        String infix = "3+4*5";
+        String infix1 = "3*4+5";
+        System.out.println(infixToPostfix(infix));
+        System.out.println(infixToPostfix(infix1));
+    }
+    static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+            if (Character.isDigit(c)) {
+                postfix.append(c);
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+        return postfix.toString();
+    }
+    private static int precedence(char operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+}
